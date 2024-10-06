9cd8fb544e0ad859bd2b287b2f2d10e162565cdc
Lau Zhan You <119853913+donkey-king-kong@users.noreply.github.com>
2024-10-06 11:24:14 +0800

    Add Spring Initializer File

---
+++ soccerZone/soccerZone/src/main/java/com/SoccerLeague/soccerZone/SoccerZoneApplication.java
@@ -0,0 +1,13 @@
+package com.SoccerLeague.soccerZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerZoneApplication.class, args);
+	}
+
+}
